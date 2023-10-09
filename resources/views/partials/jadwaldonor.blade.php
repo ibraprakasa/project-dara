1e3a0a65b7e053abcd17769cf7d6935016ef0905
afif permana <afifp04@gmail.com>
2023-10-09 16:20:39 +0700

    menambahkan google map di input jadwal

--- resources/views/partials/jadwaldonor.blade.php
+++ resources/views/partials/jadwaldonor.blade.php
@@ -7,6 +7,7 @@
         DARA || Jadwal Donor
     </title>
     <link href="../assets/css/stylepartials.css" rel="stylesheet">
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 </head>
 
 <div class="filter btn-group">
@@ -151,11 +152,14 @@
                         <label for="latitude">Latitude</label>
                         <input class="kolom form-control" name="latitude" type="number" id="latitude" name="latitude" step="any"  placeholder="ex : xx.xxx" required>
                     </div>
+                    <div id="map" style="height: 400px;"></div>
+                    </div>
+           
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-success" style="background-color: #03A13B; border-radius:10px">Tambah</button>
+                    </div>
+                </form>
             </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-success" style="background-color: #03A13B; border-radius:10px">Tambah</button>
-            </div>
-            </form>
         </div>
     </div>
 </div>
@@ -206,24 +210,19 @@
                     </div>
                     <div class="form-group" style="color:black; font-weight:bold">
                         <label for="longitude">Longitude</label>
-                        <input class="kolom form-control" type="number" id="longitude" name="longitude" value="{{ $row->longitude }}" required>
+                        <input class="kolom form-control" type="number" id="longitude" name="editlongitude" value="{{ $row->longitude }}" required>
                     </div>
                     <div class="form-group" style="color:black; font-weight:bold">
                         <label for="latitude">Latitude</label>
-                        <input class="kolom form-control" type="number" id="latitude" name="latitude" value="{{ $row->latitude }}" required>
+                        <input class="kolom form-control" type="number" id="latitude" name="editlatitude" value="{{ $row->latitude }}" required>
                     </div>
-                    <!-- <div class="form-group" style="color:black; font-weight:bold">
-                        <label for="status">Status</label>
-                        <select class="kolom form-control" id="status" ">
-                            <option class="kolom form-control" value="default">Belum Selesai</option>
-                            <option class="kolom form-control" value="selesai">Selesai</option>
-                        </select>
-                    </div> -->
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-success" style="background-color: #03A13B; border-radius:10px">Simpan</button>
-                </div>
-            </form>
+                    <div id="editmap" style="height: 400px;"></div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-success" style="background-color: #03A13B; border-radius:10px">Simpan</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
@@ -257,5 +256,52 @@
 </div>
 @endforeach
 <!-- END MODAL -->
+
+<script>
+// Menambahkan variabel untuk menyimpan referensi ke elemen input latitude dan longitude
+const editLatitudeInput = document.getElementById("latitude");
+const editLongitudeInput = document.getElementById("longitude");
+
+    function initMap() {
+  const myLatlng = { lat: -6.272945237180217, lng: 106.73903083681019 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: myLatlng,
+  });
+  // Create a marker variable to hold the marker
+  let marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    draggable: true, // Allow the marker to be dragged
+  });
+
+  // Create an info window for the marker
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Klik map untuk mendapatkan lokasi (Lat/Long)",
+    position: myLatlng,
+  });
+
+  infoWindow.open(map);
+
+  map.addListener("click", (mapsMouseEvent) => {
+    // Update the marker's position based on the click event
+    marker.setPosition(mapsMouseEvent.latLng);
+
+    // Get the latitude and longitude of the clicked location
+    const latitude = mapsMouseEvent.latLng.lat();
+    const longitude = mapsMouseEvent.latLng.lng();
+
+    // Update the latitude and longitude input fields
+    editLatitudeInput.value = latitude;
+    editLongitudeInput.value = longitude;
+  });
+
+}
+
+window.initMap = initMap;
+</script>
+<script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD0DnOUYUBmubrtiYkon5_68Q8V8L2rfn8&callback=initMap&v=weekly" defer
+    ></script>
 
 @endsection
