2f67be17c00cb65da7eece788f42fc45f4e8e185
afif permana <afifp04@gmail.com>
2023-10-09 16:29:37 +0700

    memperbaiki

--- app/Http/Controllers/JadwalDonorController.php
+++ app/Http/Controllers/JadwalDonorController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\GolonganDarah;
 use App\Models\JadwalDonor;
-use App\Models\jadwalPendonor;
+use App\Models\JadwalPendonor;
 use App\Models\Pendonor;
 use Illuminate\Http\Request;
 
