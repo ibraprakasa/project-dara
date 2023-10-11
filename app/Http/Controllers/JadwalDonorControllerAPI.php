0e1a1300dffc60b20c0c7de08245f55ea8ea03cb
afif permana <afifp04@gmail.com>
2023-10-11 12:45:24 +0700

    rabu/12:45 memperbaiki jadwal-donor-pendonor

--- app/Http/Controllers/JadwalDonorControllerAPI.php
+++ app/Http/Controllers/JadwalDonorControllerAPI.php
@@ -11,7 +11,7 @@ class JadwalDonorControllerAPI extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['show']]);
+        $this->middleware('auth:api', ['except' => []]);
     }
 
     public function show()
