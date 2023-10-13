2992952b612d6c8e8820c36ef80e5698b6e30bf7
afif permana <afifp04@gmail.com>
2023-10-13 10:24:53 +0700

    memperbaiki tambah pada berita

--- resources/views/partials/berita.blade.php
+++ resources/views/partials/berita.blade.php
@@ -91,7 +91,6 @@
 </div>
 
 <!-- MODAL INSERT BERITA -->
-@foreach($data as $row)
 <div class="modal fade tambahberita" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
@@ -127,7 +126,6 @@
         </div>
     </div>
 </div>
-@endforeach
 <!-- END MODAL -->
 
 <!-- MODAL EDIT BERITA -->
