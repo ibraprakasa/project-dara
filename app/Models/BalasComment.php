ef8549d2019100bac69c55392478772856529ffb
afif permana <afifp04@gmail.com>
2023-10-18 13:15:32 +0700

    menambahkan migrasi post dan modelnya

---
+++ app/Models/BalasComment.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+
+class BalasComment extends Authenticatable implements JWTSubject
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'balas_comments';
+    protected $primaryKey = 'id';
+    protected $guarded = [];
+
+    protected $fillable = [
+       'id_pendonor',
+       'id_comment',
+       'text'
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
