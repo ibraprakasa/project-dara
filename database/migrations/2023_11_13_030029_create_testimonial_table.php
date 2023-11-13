7b475cafd8d6fada7b8707f4c01131b930705073
Oktavianipratiwi <okta05532@gmail.com>
2023-11-13 10:44:47 +0700

    menambahkan tabel testimoni

---
+++ database/migrations/2023_11_13_030029_create_testimonial_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('testimonial', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_pendonor')->nullable(false);
+            $table->text('text');
+            $table->enum('star', ['1','2','3','4','5'])->nullable(false);
+            $table->timestamps();
+            $table->foreign('id_pendonor')->references('id')->on('pendonor');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('testimonial');
+    }
+};
