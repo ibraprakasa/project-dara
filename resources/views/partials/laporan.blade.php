048d7e73088fcb3c104110f147e890373cb150c5
Ibra Prakasa <113502572+2101082006@users.noreply.github.com>
2023-11-14 10:11:28 +0700

    perbaikan

--- resources/views/partials/laporan.blade.php
+++ resources/views/partials/laporan.blade.php
@@ -187,6 +187,31 @@
                         </div>
                     </div>
                 </div>
+                @elseif($row->posts && $row->posts->gambar == null)
+                <label style="color:red;font-weight:bold">Postingan yang dilaporkan</label>
+                <div class="form-group" style="color:black;">
+                    <textarea class="kolom form-control resizablestatus" rows="6" readonly>{{ $row->posts->text }}</textarea>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label style="color:black;font-weight:bold">Kode</label>
+                            <input class="kolom form-control" placeholder="{{ $row->posts->pendonor->kode_pendonor }}" readonly>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label style="color:black;font-weight:bold">Nama</label>
+                            <input class="kolom form-control" placeholder="{{ $row->posts->pendonor->nama }}" readonly>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label style="color:black;font-weight:bold">Tanggal Posting</label>
+                            <input class="kolom form-control" placeholder="{{ $row->posts->created_at->setTimezone('Asia/Jakarta')->translatedFormat('l, j F Y') }}" readonly>
+                        </div>
+                    </div>
+                </div>
                 @elseif ($row->comments && $row->comments->text)
                 <label style="color:red;font-weight:bold">Komentar yang dilaporkan</label>
                 <div class="form-group" style="color:black;">
