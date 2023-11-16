00cc66c1603d8e1a7ae7f7a8fa9d500107a26278
afif permana <afifp04@gmail.com>
2023-11-16 15:50:16 +0700

    menambahkan delete notifi jika post di hapus

--- app/Http/Controllers/PostControllerAPI.php
+++ app/Http/Controllers/PostControllerAPI.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Notifikasi;
 use App\Models\Pendonor;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -145,6 +146,8 @@ class PostControllerAPI extends Controller
                 unlink($gambarPath);
             }
            }
+           $notif = Notifikasi::where('id_post',$post->id)->get();
+           $notif->delete();
             $post->delete();
             return response()->json([
                 'success' => true,
