340c683f9b6d7fd3b3ddbf2ee3abe933add190b2
afif permana <afifp04@gmail.com>
2023-11-27 15:21:11 +0700

    mengembalian email otp API

---
+++ resources/views/email-otp.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ini OTP Kamu</title>
+    <!-- Tambahan metadata, stylesheet, dan script lainnya -->
+</head>
+<body>
+    OTP kamu adalah : {{ $otp }}
+</body>
+</html>
+email otp blade
