1d4781beda35dc7bf645e06e6498946b0b0a4d4b
afif permana <afifp04@gmail.com>
2023-12-27 15:55:58 +0700

    memperbaiki logika balas comment

--- app/Http/Controllers/NotifikasiControllerAPI.php
+++ app/Http/Controllers/NotifikasiControllerAPI.php
@@ -46,6 +46,7 @@ class NotifikasiControllerAPI extends Controller
                 if ($post->id_pendonor == $user->id) {
                     $comment = Comment::where('id',$notif->id_comment)->first(); // Dapatkan instance model Comment
                     $balasComment = BalasComment::where('id_comment', $comment->id_comment)->first();
+                    $balasCommentx = BalasComment::where('id_comment', $notif->id_comment)->first();
                     if ($comment->id_pendonor != $user->id && $balasComment == null) {
                         $pendonor = Pendonor::where('id', $comment->id_pendonor)->first();
                         $diff = $notif->updated_at->diffForHumans();
@@ -56,7 +57,7 @@ class NotifikasiControllerAPI extends Controller
                             'id_comment' => $notif->id_comment,
                             'id_balas_comment' => $notif->id_balas_comment,
                             'status_read' => $notif->status_read,
-                            'id_pembalas' => $balasComment->id_pendonor,
+                            'id_pembalas' => $balasCommentx->id_pendonor,
                             'pendonor' => $pendonor,
                             'update' => $diff
                         ];
