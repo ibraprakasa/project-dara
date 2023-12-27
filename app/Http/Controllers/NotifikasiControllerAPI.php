d3b7f2eca7dda3699444a512780962d64177113f
afif permana <afifp04@gmail.com>
2023-12-27 12:36:45 +0700

    memperbaiki logika balas comment

--- app/Http/Controllers/NotifikasiControllerAPI.php
+++ app/Http/Controllers/NotifikasiControllerAPI.php
@@ -67,17 +67,19 @@ class NotifikasiControllerAPI extends Controller
                     $balasComment = BalasComment::where('id', $notif->id_balas_comment)->first(); // Dapatkan instance model BalasComment
                     if ($balasComment && $balasComment->id_pendonor != $user->id) {
                         $pendonor = Pendonor::where('id', $balasComment->id_pendonor)->first();
-                        $diff = $notif->updated_at->diffForHumans();
-                        $diff = str_replace('dari sekarang', 'yang lalu', $diff);
-                        $responseData[] = [
-                            'id' => $notif->id,
-                            'id_post' => $notif->id_post,
-                            'id_comment' => $notif->id_comment,
-                            'id_balas_comment' => $notif->id_balas_comment,
-                            'status_read' => $notif->status_read,
-                            'pendonor' => $pendonor,
-                            'update' => $diff
-                        ];
+                        if($pendonor->id != $user->id){
+                            $diff = $notif->updated_at->diffForHumans();
+                            $diff = str_replace('dari sekarang', 'yang lalu', $diff);
+                            $responseData[] = [
+                                'id' => $notif->id,
+                                'id_post' => $notif->id_post,
+                                'id_comment' => $notif->id_comment,
+                                'id_balas_comment' => $notif->id_balas_comment,
+                                'status_read' => $notif->status_read,
+                                'pendonor' => $pendonor,
+                                'update' => $diff
+                            ];
+                        }
                     }
                 }
             }
